b258a96daf3733d9abe9c9e467e0cf40be85ac64
karol-ciok <karolmc02@gmail.com>
2017-02-17 15:37:33 +0100

    Añadidos los wrappers de los servicios

--- src/main/java/es/uniovi/asw/participants/controller/ParticipantController.java
+++ src/main/java/es/uniovi/asw/participants/controller/ParticipantController.java
@@ -3,48 +3,38 @@ package es.uniovi.asw.participants.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.uniovi.asw.dbManagement.model.Participant;
+import es.uniovi.asw.participants.wrappers.LoginWrapper;
 import es.uniovi.asw.participants.wrappers.ParticipantInfo;
+import es.uniovi.asw.participants.wrappers.UpdatePassWrapper;
 
 @Controller
 @RestController
 public class ParticipantController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST, produces = {
-	    MediaType.APPLICATION_JSON_VALUE,
-	    MediaType.APPLICATION_XML_VALUE }, consumes = {
-		    MediaType.APPLICATION_JSON_VALUE,
-		    MediaType.APPLICATION_XML_VALUE,
-		    MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
     public ResponseEntity<ParticipantInfo> queryInfo(
-	    @RequestBody @Valid final MultiValueMap<String, String> paramMap) {
+	    @RequestBody @Valid final LoginWrapper loginWrapper) {
 
-	if (paramMap == null)
+	if (loginWrapper == null)
 	    return new ResponseEntity<ParticipantInfo>(HttpStatus.BAD_REQUEST);
 
 	return new ResponseEntity<ParticipantInfo>(
 		new ParticipantInfo(new Participant("Nombre-Ejemplo",
-			"Contraseña-Ejemplo", paramMap.getFirst("login"))),
+			"Contraseña-Ejemplo", loginWrapper.getLogin())),
 		HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/changePass", method = RequestMethod.POST, produces = {
-	    MediaType.APPLICATION_JSON_VALUE,
-	    MediaType.APPLICATION_XML_VALUE }, consumes = {
-		    MediaType.APPLICATION_JSON_VALUE,
-		    MediaType.APPLICATION_XML_VALUE,
-		    MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+    @RequestMapping(value = "/changePass", method = RequestMethod.POST)
     public void updateInfo(
-	    @RequestBody @Valid final MultiValueMap<String, String> paramMap) {
+	    @RequestBody @Valid final UpdatePassWrapper updateWrapper) {
 	// TODO Auto-generated method stub
 
     }
