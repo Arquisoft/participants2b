c027dd7492f89b127fb826c4e6833362a9967bc5
UO245226 <aveco96@hotmail.com>
2017-02-22 18:10:28 +0100

    Actualizada base de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO Participants (nombre, apellidos, email, password, fecha_nacimiento, residencia, nacionalidad, ndi)
-VALUES ('John', 'Doe', 'john@doe.com', 'password', '1994-10-10', 'Nada', 'Data', '123'),
- ('John', 'Doe', 'john2@doe.com', 'password', '1994-10-10', 'Nada', 'Data', '123');
+VALUES ('John', 'Doe', 'john@doe.com', 'john123', '1994-10-10', 'Oviedo', 'Italia', '12345'),
+('Iker', 'Jimenez', 'iker@doe.com', 'iker123', '1990-10-10', 'Oviedo', 'Italia', '22113'),
+('Fernando', 'Alonso', 'fernando@doe.com', 'fernando123', '1985-10-10', 'Oviedo', 'Italia', '95666'),
+('Paco', 'Fernandez', 'paco@doe.com', 'paco123', '1960-10-10', 'Oviedo', 'Italia', '1'),
+('Andreu', 'Buenafuente', 'andreu@doe.com', 'andreu123', '1961-10-10', 'Oviedo', 'Italia', '462799'),
+('Berto', 'Romero', 'berto@doe.com', 'berto123', '1963-10-10', 'Oviedo', 'Italia', '622104'),
+('Carlos', 'Sainz', 'carlos@doe.com', 'carlos123', '1970-10-10', 'Oviedo', 'Italia', '406485'),
+('Lewis', 'Hamilton', 'lewis@doe.com', 'lewis123', '1980-10-10', 'Oviedo', 'Italia', '6641959');
